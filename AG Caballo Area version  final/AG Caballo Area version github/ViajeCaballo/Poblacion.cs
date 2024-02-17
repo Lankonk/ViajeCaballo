4c1aba3a062432432f7c0611718836f3111bc17e
Luis Islas <160322287+Lankonk@users.noreply.github.com>
2024-02-17 16:45:40 -0600

    Codigo original
    El que no dice VSCode solo abre en visual studio.

---
+++ AG Caballo Area version  final/AG Caballo Area version github/ViajeCaballo/Poblacion.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViajeCaballo
+{
+    internal class Poblacion
+    {
+        public Organismo MejorOrganismo;
+        public Organismo [] Population=new Organismo [4000];//Si funciona baja a 2000
+        public Organismo[] PopulationOrdenada = new Organismo[4000];//jola
+        public void PrimeraGeneracion(string PrimeraCasilla, Random r)
+        {
+            string letra=PrimeraCasilla.TrimEnd('1','2','3','4','5','6','7','8'), numero=PrimeraCasilla.TrimStart('A','B','C','D','E','F','G','H','a','b','c','d','e','f','g','h');
+            letra = letra.ToUpper();
+            switch(letra)
+            {
+                case "A":
+                    letra = "1";
+                    break;
+                case "B":
+                    letra = "2";
+                    break;
+                case "C":
+                    letra = "3";
+                    break;
+                case "D":
+                    letra = "4";
+                    break;
+                case "E":
+                    letra = "5";
+                    break;
+                case "F":
+                    letra = "6";
+                    break;
+                case "G":
+                    letra = "7";
+                    break;
+                case "H":
+                    letra = "8";
+                    break;
+            }
+            for (int i = 0; i < 4000; i++)
+            {
+                Organismo org = new Organismo();
+                org.CromosomaAleatorio(r);
+                org.letra = Int16.Parse(letra);
+                org.numero = Int16.Parse(numero);
+                org.CalcularAdecuacion();
+                Population[i] = org;
+            }
+            CalcularMejorOrganismo();
+        }
+        public void ImprimirPoblacion(int contador)//Cambio a Ordenada
+        {
+            for (int i = 0; i < 2000; i++)   
+             PopulationOrdenada[i].ImprimirCromosoma(contador);             
+        }
+        public void ImprimirMejores(int contador)//Cambio a Ordenada
+        {
+            for (int i = 0; i < 2000; i++)            
+                if (PopulationOrdenada[i].adecuacion > 0)// Nada más se imprimen los mejores
+                    PopulationOrdenada[i].ImprimirCromosoma(contador);
+            
+        }
+        public void CalcularMejorOrganismo()
+        {       
+            MejorOrganismo = Population[0];
+            for (int i=1;i<4000;i++)
+                if(Population[i].adecuacion> MejorOrganismo.adecuacion)
+                    MejorOrganismo= Population[i];
+            
+        }
+        public void Ordenar()
+        {           
+            int contador = 0;
+            for (int x = MejorOrganismo.adecuacion; x >-1;x--)
+            for (int i = 0;i<4000;i++)//jola
+            {
+                    if (Population[i].adecuacion == x)
+                    {
+                        PopulationOrdenada[contador] = Population[i];
+                        contador++;
+                    }
+            }
+        }
+    }
+
+}
---
+++ AG Caballo Area version  final/AG Caballo Area version github/ViajeCaballo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViajeCaballo
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Random r =new Random();
+            string PrimeraCasilla="";
+            Console.WriteLine("Introduce la casilla de inicio: Una letra de la A-H seguida de un número (sin espacio) del 1-8.");
+            Console.WriteLine("ej. A1");
+            PrimeraCasilla = Console.ReadLine();
+
+            AlgoritmoGenetico AG=new AlgoritmoGenetico();
+            AG.Empezar(PrimeraCasilla, r);
+            Console.WriteLine("La secuencia ha sido encontrada, es la última entrada del archivo de texto.");
+            Console.ReadKey();
+        }
+    }
+}
